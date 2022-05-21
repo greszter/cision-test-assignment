a8051c6a3a354ba94fa96d753bd16f9bbab47b9f
greszter <esztergreskovics@gmail.com>
2022-05-21 20:05:52 +0200

    Add get all items endpoint

--- src/main/java/com/example/cision/controller/PalindromeController.java
+++ src/main/java/com/example/cision/controller/PalindromeController.java
@@ -1,25 +1,33 @@
 package com.example.cision.controller;
 
 import com.example.cision.model.PalindromeInput;
+import com.example.cision.model.PalindromeResponse;
+import com.example.cision.repository.PalindromeRepository;
 import com.example.cision.service.PalindromeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class PalindromeController {
 
     @Autowired
-    PalindromeService service;
+    private PalindromeService service;
+
+    @Autowired
+    private PalindromeRepository repository;
 
     @PostMapping("add-new-item")
-    public ResponseEntity<String> addNew(@Valid PalindromeInput input) {
-        service.addNew(input);
-        return ResponseEntity.ok("New item successfully saved");
+    public ResponseEntity<PalindromeResponse> addNew(@Valid @RequestBody PalindromeInput input) {
+        return ResponseEntity.ok(service.addNew(input));
+    }
+
+    @GetMapping("get-all-items")
+    public ResponseEntity<List<PalindromeResponse>> getAll() {
+        return ResponseEntity.ok(repository.findAll());
     }
 }
