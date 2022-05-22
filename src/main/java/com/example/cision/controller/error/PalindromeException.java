6cf6f8547ae3bbd89fbbdf01f36d5a522ad24aa7
greszter <esztergreskovics@gmail.com>
2022-05-22 13:45:57 +0200

    Add custom exception

--- src/main/java/com/example/cision/controller/error/PalindromeException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cision.exception;
-
-public class PalindromeException extends RuntimeException{
-    public IncorrectFileExtensionException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-}
